ab3119d38cc50d0573e1e0b936e786f5d8a1622b
HengChang <HengChang@DESKTOP-RR9KCCH>
2017-04-06 15:46:59 +0800

    hw1_xml analysis update

--- 1. XML_Analysis/1. XML_Analysis/Program.cs
+++ 1. XML_Analysis/1. XML_Analysis/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace _1.XML_Analysis
 {
@@ -10,6 +11,41 @@ namespace _1.XML_Analysis
     {
         static void Main(string[] args)
         {
+            var xml = XElement.Load(@"http://opendata.epa.gov.tw/ws/Data/UV/?format=xml");
+
+            var UV = xml.Descendants("Data");
+
+            UV.ToList().ForEach(uv =>
+           {
+                var SiteName = uv.Element("SiteName").Value.Trim();
+                var UVI = uv.Element("UVI").Value.Trim();
+                var PublishAgency = uv.Element("PublishAgency").Value.Trim();
+                var County = uv.Element("County").Value.Trim();
+                var WGS84Lon = uv.Element("WGS84Lon").Value.Trim();
+                var WGS84Lat = uv.Element("WGS84Lat").Value.Trim();
+                var PublishTime = uv.Element("PublishTime").Value.Trim();
+
+               Station station = new Station();
+               station.SiteName = SiteName;
+               station.UVI = UVI;
+               station.PublishAgency = PublishAgency;
+               station.County = County;
+               station.WGS84Lon = WGS84Lon;
+               station.WGS84Lat = WGS84Lat;
+               station.PublishTime = PublishTime;
+
+               Console.Write(station.SiteName + "  ");
+               Console.Write(station.UVI + "  ");
+               Console.Write(station.PublishAgency + "  ");
+               Console.Write(station.County + "  ");
+               Console.Write(station.WGS84Lon + "  ");
+               Console.Write(station.WGS84Lat + "  ");
+               Console.WriteLine(station.PublishTime);
+               
+
+           });
+
+            Console.Read();
         }
     }
 }
